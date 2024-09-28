7c3f678313b324e984d9e2a57b8db1a8886d3840
Sebastian <sebastian@coderlabs.co>
2024-09-28 12:04:08 -0400

    feat:: init project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { NavbarDefault } from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+    <NavbarDefault/>
+    <div className='bg-red-500'>oo</div>
+    </>
+  )
+}
+
+export default App
