effb05bb2d24444d1bf5e1024380357a3ddc7111
Luis Correa <luiscorrea874@gmail.com>
2024-10-04 22:38:58 -0500

    orders

--- src/components/Admin-panel/Orders.jsx
+++ src/components/Admin-panel/Orders.jsx
@@ -1,5 +1,57 @@
+import React, { useEffect, useState } from "react";
+
 const Orders = () => {
-  return <div>Orders</div>;
+  const [orders, setOrders] = useState([]);
+  const [error, setError] = useState(null);
+
+  const fetchOrders = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/product/orders", {
+        method: "GET",
+      });
+
+      if (!response.ok) {
+        throw new Error("Error fetching orders");
+      }
+
+      const data = await response.json();
+
+      if (Array.isArray(data.orders)) {
+        setOrders(data.orders);
+      } else {
+        throw new Error("Unexpected response format");
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  return (
+    <div className="bg-gray-800 p-6 rounded-lg shadow-md">
+      <h1 className="text-2xl font-bold text-white mb-4">Orders</h1>
+      {error && <p className="text-red-500 mb-4">Error: {error}</p>}
+      <ul className="space-y-2">
+        {Array.isArray(orders) && orders.length > 0 ? (
+          orders.map((order) => (
+            <li
+              key={order._id}
+              className="bg-gray-700 p-4 rounded-lg hover:bg-gray-600 transition duration-200"
+            >
+              <span className="text-white">
+                {`Order Number: ${order.orderNumber} - Price: $${order.price}`}
+              </span>
+            </li>
+          ))
+        ) : (
+          <p className="text-white">No orders available.</p>
+        )}
+      </ul>
+    </div>
+  );
 };
 
 export default Orders;
