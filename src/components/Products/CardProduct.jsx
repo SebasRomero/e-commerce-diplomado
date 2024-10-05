c6f7f409d592958bec236232d336944d71f1625d
Sebastian <sebastian@coderlabs.co>
2024-10-05 05:41:40 -0400

    change:: color buttons

--- src/components/Products/CardProduct.jsx
+++ src/components/Products/CardProduct.jsx
@@ -36,7 +36,7 @@ export const CardProduct = (props) => {
           </span>
           <button
             onClick={handleAddToCart} // Ensure this line calls handleAddToCart
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="text-white bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
           >
             Add to cart
           </button>
